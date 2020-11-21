8e2744e9c0612d8086c1c6c8e94713b3a814900d
brennandoehring <nannerbdale@gmail.com>
2020-11-21 06:37:31 -0500

    Create post button working in home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-  const createPost = $(".create-btn");
+  const createPost = $("#create");
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
